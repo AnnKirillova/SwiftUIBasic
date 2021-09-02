ded10df882bd9815809cc1e6dce839b29a283622
Ann <anut.kirillova@gmail.com>
2021-09-02 20:35:06 +0300

    Did complex interface

--- FirstUIApp/FirstUIApp/Model/ModelData.swift
+++ FirstUIApp/FirstUIApp/Model/ModelData.swift
@@ -11,6 +11,16 @@ import Combine
 final class ModelData: ObservableObject{
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+            landmarks.filter { $0.isFeatured }
+        }
+    
+    var categories: [String: [Landmark]]{
+        Dictionary(grouping: landmarks) { landmark in
+            landmark.category.rawValue
+        }
+    }
 }
 
 func load<T: Decodable>(_ fileName: String) -> T{
